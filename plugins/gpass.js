8b0110c11a30ecb9cbc49cd1b8797856ac3d1c56
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-16 13:38:33 +0530

    Update gpass.js

--- plugins/gpass.js
+++ plugins/gpass.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         };
 
         const password = generatePassword(length);
-        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n> *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`;
+        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n> *©🐼❤️‍🔥ッ*`;
 
         // Send initial notification message
         await conn.sendMessage(from, { text: message }, { quoted: mek });
