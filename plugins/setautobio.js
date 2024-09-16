72193c449399dad5ba84e2a65a6006bb853cae39
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-16 14:05:03 +0530

    Update setautobio.js

--- plugins/setautobio.js
+++ plugins/setautobio.js
@@ -33,7 +33,7 @@ function startAutoBio(conn) {
     // Set a new interval to update the bio every minute (or any preferred time)
     autoBioInterval = setInterval(async () => {
         const time = new Date().toLocaleTimeString();  // Get the current time
-        const bioText = `🌟 QUEEN-KYLIE-MD [${time}] 🌟`;  // Set the bio text with time
+        const bioText = `🌟 මම මෝඩයෙක්😾🫴 [${time}] 🌟`;  // Set the bio text with time
         await conn.updateProfileStatus(bioText);  // Update the bot's bio
     }, 60 * 1000);  // 1 minute interval
 }
