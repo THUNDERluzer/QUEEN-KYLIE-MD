3cd5cff62c4aaa749969ba6f500034a26409a2bf
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-16 13:34:16 +0530

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *ʀᴀɴᴅᴏᴍ ᴅᴏɢ ɪᴍᴀɢᴇ* 🐶\n> *©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *ʀᴀɴᴅᴏᴍ ᴅᴏɢ ɪᴍᴀɢᴇ* 🐶\n> *©🐼❤️‍🔥ッ*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
