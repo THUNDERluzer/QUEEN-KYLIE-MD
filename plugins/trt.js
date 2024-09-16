e004bd2ebc572802a0395212acace7cdd84bdfab
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-16 14:14:56 +0530

    Update trt.js

--- plugins/trt.js
+++ plugins/trt.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 🔠 *Translated*: ${translation}
 🌐 *Language*: ${targetLang.toUpperCase()}
 
-*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`;
+*©🐼❤️‍🔥ッ*`;
 
         return reply(translationMessage);
     } catch (e) {
