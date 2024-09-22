f31c5e08dcb17cba89ca41c6603f10754c648ed2
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-22 10:30:23 +0530

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -10,7 +10,7 @@ let baseUrl;
 })();
 
 
-const yourName = "*©🐼❤️‍🔥ッ*";
+const yourName = "*wa.me/94782543893☠️💖*";
 
 
 
@@ -54,7 +54,7 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
         let data = await fetchJson(`${baseUrl}/api/tiktokdl?url=${q}`)
         reply("*Downloading...*")
         //send video (wm,nwm)
-        await conn.sendMessage(from, { video: { url: data.data.no_wm }, mimetype: "video/mp4", caption: `- NO-WATERMARK\n\n ${yourName}` }, { quoted: mek })
+        await conn.sendMessage(from, { video: { url: data.data.no_wm }, mimetype: "video/mp4", caption: `> Hashan tiktok video Downloader\n\n ${yourName}` }, { quoted: mek })
         await conn.sendMessage(from, { video: { url: data.data.wm }, mimetype: "video/mp4", caption: `- WITH-WATERMARK \n\n ${yourName}` }, { quoted: mek })  
         //send audio    
         await conn.sendMessage(from, { audio: { url: data.data.audio }, mimetype: "audio/mpeg" }, { quoted: mek })  
